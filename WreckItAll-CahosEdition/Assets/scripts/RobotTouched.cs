92534817a1d8d0bb7d7837f85811c234a3b2b675
Arnaud63 <a.allemand0@gmail.com>
2022-03-11 14:32:18 +0100

    Changement de la méthode de collision des Robots avec les IWeapons
    Les collisions entre les ennemis et les IWeapons sont désormais détectées via les script LimbCollisionController qui sont placées sur les limbs des robots

--- WreckItAll-CahosEdition/Assets/scripts/RobotTouched.cs
+++ WreckItAll-CahosEdition/Assets/scripts/RobotTouched.cs
@@ -19,34 +19,42 @@ public class RobotTouched : MonoBehaviour
         this.ToggleAnimation();
         ChangeIsKinematicState(true);
         ChangeCollisionDetectionMode(CollisionDetectionMode.ContinuousSpeculative);
+        AddLimbCollisionOnChild();
     }
 
+    /*
     private void OnCollisionEnter(Collision collision)
     {
         if (enemysManager.ContainsGameObject(collision.collider.gameObject.transform.root.gameObject)) return;
         //Devrait y avoir un test si c'est une IWeapon mais ça ne fonctionne pas, tout ce qui entre en
         //collision avec l'ennemy est considéré comme pas une IWeapon
         enemysManager.DestroyLinkGameObjectIEnemy(collision.collider.gameObject);
-        OnTouched();
+        OnTouched(collision.impulse, collision.gameObject);
+    }
+    */
+
+    public void OnCollisionFromChild(Collision collision, Rigidbody limbCollided)
+    {
+        if (enemysManager.ContainsGameObject(collision.collider.gameObject.transform.root.gameObject)) return;
+        //Devrait y avoir un test si c'est une IWeapon mais ça ne fonctionne pas, tout ce qui entre en
+        //collision avec l'ennemy est considéré comme pas une IWeapon
+        enemysManager.DestroyLinkGameObjectIEnemy(collision.collider.gameObject);
+        OnTouched(collision.relativeVelocity, limbCollided);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //ONLY FOR TEST PURPOSE
-        if(Input.GetKey("d"))
-        {
-            this.OnTouched();
-        }
+
     }
 
-    public void OnTouched()
+    public void OnTouched(Vector3 relativeVelocity, Rigidbody limbCollided)
     {
-        this.ToggleRagdoll();
+        this.ToggleRagdoll(relativeVelocity, limbCollided);
         Destroy(this.gameObject, DESPAWN_TIME);
     }
     
-    private void ToggleRagdoll()
+    private void ToggleRagdoll(Vector3 relativeVelocity, Rigidbody limbCollided)
     {
         //this.ChangeCollidersState(true);
         this.GetComponent<Animator>().enabled = false;
@@ -54,6 +62,7 @@ public class RobotTouched : MonoBehaviour
         this.GetComponent<animationStateController>().enabled = false;
         this.GetComponent<NavMeshAgent>().enabled = false;
         this.GetComponent<NavMeshAgentController>().enabled = false;
+        limbCollided.AddForce(relativeVelocity * 50);
     }
 
     private void ToggleAnimation()
@@ -89,6 +98,14 @@ public class RobotTouched : MonoBehaviour
         foreach (Rigidbody rb in gameObject.GetComponentsInChildren<Rigidbody>())
         {
             rb.collisionDetectionMode = detect_mode;
+        }
+    }
+
+    private void AddLimbCollisionOnChild()
+    {
+        foreach (Transform tr in gameObject.GetComponentsInChildren<Transform>())
+        {
+            tr.gameObject.AddComponent<LimbCollisionController>();
         }
     }
 }
